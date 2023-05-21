3485559b75c61034d53336a6d0eda22ba2f671a4
Michał Mroczek <mroczekmichal57@gmail.com>
2023-05-21 16:44:26 +0200

    view for signing up

---
+++ src/components/SignUp.vue
@@ -0,0 +1,146 @@
+<template>
+  <h1 class="text-4xl pb-5">Sign Up</h1>
+  <div class="place-items-center grid">
+    <form
+      v-on:submit="signUp"
+      class="bg-white shadow-md rounded px-8 pb-8 mb-4"
+      ref="form"
+    >
+      <div class="mb-4">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
+          Name
+        </label>
+        <input
+          v-model="name"
+          ref="name"
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="name"
+          type="text"
+          placeholder="Name"
+          name="name"
+          required
+        />
+      </div>
+      <div class="mb-6">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+          Email
+        </label>
+        <input
+          v-model="email"
+          ref="email"
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          id="email"
+          type="email"
+          placeholder="Email"
+          name="email"
+          required
+        />
+        <p ref="error_message_mail" class="text-red-500 text-xs italic"></p>
+      </div>
+      <div class="mb-6">
+        <label
+          class="block text-gray-700 text-sm font-bold mb-2"
+          for="password"
+        >
+          Password
+        </label>
+        <input
+          v-model="password"
+          minlength="6"
+          ref="password"
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          id="password"
+          type="password"
+          placeholder="******************"
+          name="password"
+          required
+        />
+        <p ref="error_message_password" class="text-red-500 text-xs italic"></p>
+      </div>
+      <div class="mb-6">
+        <label
+          class="block text-gray-700 text-sm font-bold mb-2"
+          for="password_confirmation"
+        >
+          Password Confirmation
+        </label>
+        <input
+          v-model="password_confirmation"
+          minlength="6"
+          ref="password_confirmation"
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          id="password_confirmation"
+          type="password"
+          placeholder="******************"
+          name="password_confirmation"
+          required
+        />
+        <p ref="error_message_password_confirmation" class="text-red-500 text-xs italic"></p>
+      </div>
+      <p ref="error_message" class="text-red-500 text-xs italic"></p>
+      <div class="pt-2">
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="submit"
+        >
+          Sign Up
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "SignUp",
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+      password_confirmation: "",
+    };
+  },
+  methods: {
+    async signUp(event) {
+      event.preventDefault();
+
+      try {
+        let result = await axios.post(
+          "http://e_managament.dvl.to/api/auth/register",
+          {
+            name: this.name,
+            email: this.email,
+            password: this.password,
+            password_confirmation: this.password_confirmation,
+          }
+        );
+        alert(result);
+      } catch (error) {
+        let errorData = JSON.parse(error.response.data);
+        let errorMailParagraf = this.$refs.error_message_mail;
+        let errorPasswordParagraf = this.$refs.error_message_password;
+        let errorPasswordConfirmationParagraf = this.$refs.error_message_password_confirmation;
+
+        if (errorData.email) {
+          let emailInput = this.$refs.email;
+
+          emailInput.classList.add("border-red-500");
+          errorMailParagraf.textContent = errorData.email;
+        }
+
+        if (errorData.password) {
+          let passwordInput = this.$refs.password;
+          let passwordConfirmationInput = this.$refs.password_confirmation;
+
+          passwordInput.classList.add("border-red-500");
+          passwordConfirmationInput.classList.add("border-red-500");
+          errorPasswordParagraf.textContent = errorData.password;
+          errorPasswordConfirmationParagraf.textContent = errorData.password;
+        }
+      }
+    },
+  },
+};
+</script>
