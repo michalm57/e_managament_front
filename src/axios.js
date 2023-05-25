43a1f3b50d45810e7c41aa5eef2331a8a7744c2d
Michał Mroczek <mroczekmichal57@gmail.com>
2023-05-25 19:05:32 +0200

    add missing Bearer in axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = "http://e_managament.dvl.to/api/";
-axios.defaults.headers.common['Authorization'] = localStorage.getItem('token');
+axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
