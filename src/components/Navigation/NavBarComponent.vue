095b4ca3558150a80d1ce7c963006c8291b50e02
Michał Mroczek <mroczekmichal57@gmail.com>
2023-05-25 19:05:41 +0200

    upgrading navbar

--- src/components/Navigation/NavBarComponent.vue
+++ src/components/Navigation/NavBarComponent.vue
@@ -3,7 +3,7 @@
     <div
       class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
     >
-      <a href="/" class="flex items-center">
+      <router-link to="/" class="flex items-center">
         <img
           src="https://images.freeimages.com/images/previews/09e/moon-art-1641879.png"
           class="h-8 mr-3"
@@ -13,53 +13,44 @@
           class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
           >E-Managament</span
         >
-      </a>
-      <button
-        data-collapse-toggle="navbar-default"
-        type="button"
-        class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-        aria-controls="navbar-default"
-        aria-expanded="false"
-      >
-        <span class="sr-only">Open main menu</span>
-        <svg
-          class="w-6 h-6"
-          aria-hidden="true"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-            clip-rule="evenodd"
-          ></path>
-        </svg>
-      </button>
+      </router-link>
       <div class="hidden w-full md:block md:w-auto" id="navbar-default">
         <ul
           class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
         >
           <li>
-            <a
+            <router-link
+              to="/"
               href="#"
               class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               aria-current="page"
-              ><router-link to="/">Home</router-link></a
+            >
+              Home</router-link
             >
           </li>
-          <li>
+          <li v-if="user">
             <a
-              href="#"
+              href="javascript:void(0)"
               class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              ><router-link to="/login">Login</router-link></a
+              aria-current="page"
+              @click="handleClick"
+            >
+              Logout</a
             >
           </li>
-          <li>
-            <a
+          <li v-if="!user">
+            <router-link
+              to="/login" 
+              class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >Login</router-link
+            >
+          </li>
+          <li v-if="!user">
+            <router-link
+              to="/sign-up"
               href="#"
               class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              ><router-link to="/sign-up">Sign Up</router-link></a
+              >Sign Up</router-link
             >
           </li>
         </ul>
@@ -69,7 +60,19 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 export default {
   name: "NavBarComponent",
+
+  methods: {
+    handleClick(){
+      localStorage.removeItem('token');
+      this.$store.dispatch('user', null);
+      this.$router.push('/');
+    }
+  },
+  computed: {
+    ...mapGetters(['user'])
+  }
 };
 </script>
