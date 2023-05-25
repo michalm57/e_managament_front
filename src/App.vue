fb3382d26a9d42a11423148e6e474986647abc2e
Michał Mroczek <mroczekmichal57@gmail.com>
2023-05-25 19:04:49 +0200

    install and cofigure vuex

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <template>
-  <nav-bar-component/>  
-  <router-view />
+  <nav-bar-component/>
+  <router-view/>
 </template>
 
 <script>
-import NavBarComponent from './components/Navigation/NavBarComponent.vue';
+import NavBarComponent from "./components/Navigation/NavBarComponent.vue";
+import axios from "axios";
 export default {
   name: "App",
   components: {
     NavBarComponent,
-  }
+  },
+  async created() {
+    try {
+      const respone = await axios.get("auth/user");
+
+      if (respone.status === 200) {
+        this.$store.dispatch("user", respone.data);
+      }
+    } catch (error) {
+      this.$router.push({ name: "LoginPage" });
+    }
+  },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import { createApp } from "vue";
+import { store } from './vuex.js';
 import App from "./App.vue";
 import router from "./routers.js";
 import './axios.js'
 
 import "./assets/style.css";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+app.use(router);
+app.use(store);
+app.mount('#app');
