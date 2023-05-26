060da6cfc095a394f7373759a008a48e69f582a7
Michał Mroczek <mroczekmichal57@gmail.com>
2023-05-26 17:28:29 +0200

    confgure main.js, store.js and

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import { createApp } from "vue";
-import { store } from './vuex.js';
 import App from "./App.vue";
-import router from "./routers.js";
+import router from "./routers";
+import store from "./store";
 import './axios.js'
 
 import "./assets/style.css";
 
 const app = createApp(App);
+
 app.use(router);
 app.use(store);
-app.mount('#app');
+
+app.mount("#app");
---
+++ src/store.js
@@ -0,0 +1,26 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+  state() {
+    return {
+      user: null,
+    };
+  },
+  getters: {
+    user: (state) => {
+      return state.user;
+    },
+  },
+  actions: {
+    user(context, user) {
+      context.commit("user", user);
+    },
+  },
+  mutations: {
+    user(state, user) {
+      state.user = user;
+    },
+  },
+});
+
+export default store;
