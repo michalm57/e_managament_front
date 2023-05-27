155ee2e6cdd87ecbbd8b4309eb5793e58e5ee9cf
Michał Mroczek <mroczekmichal57@gmail.com>
2023-05-27 18:23:55 +0200

    base burger menu

--- src/components/Navigation/NavBarComponent.vue
+++ src/components/Navigation/NavBarComponent.vue
@@ -60,6 +60,66 @@
           </li>
         </ul>
       </div>
+      <div class="md:hidden flex items-center">
+        <button class="outline-none mobile-menu-button">
+          <svg
+            class="w-6 h-6 text-gray-500 hover:text-red-400"
+            x-show="!showMenu"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path d="M4 6h16M4 12h16M4 18h16"></path>
+          </svg>
+        </button>
+      </div>
+      <div class="hidden mobile-menu">
+        <ul>
+          <li class="active">
+            <router-link
+              li
+              v-if="user"
+              to="/"
+              href="#"
+              class="block text-sm px-2 py-4 text-white font-semibold"
+              aria-current="page"
+              :class="{ 'text-red-400': currentRouteName === 'HomePage' }"
+            >
+              Home</router-link
+            >
+          </li>
+          <li v-if="user">
+            <a
+              href="javascript:void(0)"
+              class="block text-sm px-2 py-4 transition duration-300"
+              aria-current="page"
+              @click="handleClick"
+            >
+              Logout</a
+            >
+          </li>
+          <li v-if="!user">
+            <router-link
+              to="/login"
+              class="block text-sm px-2 py-4 transition duration-300"
+              :class="{ 'text-red-400': currentRouteName === 'LoginPage' }"
+              >Login</router-link
+            >
+          </li>
+          <li v-if="!user">
+            <router-link
+              to="/sign-up"
+              href="#"
+              class="block text-sm px-2 py-4 transition duration-300"
+              :class="{ 'text-red-400': currentRouteName === 'SignUpPage' }"
+              >Sign Up</router-link
+            >
+          </li>
+        </ul>
+      </div>
     </div>
   </nav>
 </template>
@@ -86,6 +146,17 @@ export default {
     currentRouteName() {
       return this.$route.name;
     },
+  },
+  mounted() {
+    const initializeMenu = () => {
+      const btn = document.querySelector("button.mobile-menu-button");
+      const menu = document.querySelector(".mobile-menu");
+      btn.addEventListener("click", () => {
+        menu.classList.toggle("hidden");
+      });
+    };
+
+    initializeMenu();
   },
 };
 </script>
