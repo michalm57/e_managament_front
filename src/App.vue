257857e4bd919f414f461751a0363cc61f048c0d
Michał Mroczek <mroczekmichal57@gmail.com>
2023-05-27 18:53:56 +0200

    move created to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
 <template>
-  <nav-bar-component/>
-  <router-view/>
+  <nav-bar-component />
+  <router-view />
 </template>
 
 <script>
 import NavBarComponent from "./components/Navigation/NavBarComponent.vue";
+import axios from "axios";
 export default {
   name: "App",
   components: {
     NavBarComponent,
-  }
+  },
+  async created() {
+    try {
+      const respone = await axios.get("auth/user");
+
+      if (respone.status === 200) {
+        this.$store.dispatch("user", respone.data);
+      }
+    } catch {
+      console.log("test");
+    }
+  },
 };
 </script>
--- src/components/Dashboard/HomePage.vue
+++ src/components/Dashboard/HomePage.vue
@@ -7,22 +7,10 @@
 
 <script>
 import { mapGetters } from "vuex";
-import axios from "axios";
 export default {
   name: "HomePage",
   computed: {
     ...mapGetters(["user"]),
-  },
-  async created() {
-    try {
-      const respone = await axios.get("auth/user");
-
-      if (respone.status === 200) {
-        this.$store.dispatch("user", respone.data);
-      }
-    } catch (error) {
-      this.$router.push({ name: "LoginPage" });
-    }
-  },
+  }
 };
 </script>
