61b8e57f7068bad12e77c395132ddda62402bc2f
Michał Mroczek <mroczekmichal57@gmail.com>
2023-05-27 18:23:45 +0200

    move dispatch of suer to HomePage

--- src/App.vue
+++ src/App.vue
@@ -5,22 +5,10 @@
 
 <script>
 import NavBarComponent from "./components/Navigation/NavBarComponent.vue";
-import axios from "axios";
 export default {
   name: "App",
   components: {
     NavBarComponent,
-  },
-  async created() {
-    try {
-      const respone = await axios.get("auth/user");
-
-      if (respone.status === 200) {
-        this.$store.dispatch("user", respone.data);
-      }
-    } catch (error) {
-      this.$router.push({ name: "LoginPage" });
-    }
-  },
+  }
 };
 </script>
--- src/components/Dashboard/HomePage.vue
+++ src/components/Dashboard/HomePage.vue
@@ -6,11 +6,23 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters } from "vuex";
+import axios from "axios";
 export default {
   name: "HomePage",
   computed: {
-    ...mapGetters(['user'])
+    ...mapGetters(["user"]),
+  },
+  async created() {
+    try {
+      const respone = await axios.get("auth/user");
+
+      if (respone.status === 200) {
+        this.$store.dispatch("user", respone.data);
+      }
+    } catch (error) {
+      this.$router.push({ name: "LoginPage" });
+    }
   },
 };
 </script>
